be58adf4a0815fb859f37a166ede00f300d906ec
Karishma <karishma@valardigital.com>
2021-03-14 18:09:55 +0530

    Project Created

---
+++ Auction/models.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.utils import timezone
+from django.db import models
+
+# Create your models here.
+
+class Auctions(models.Model):
+    name = models.CharField(max_length=128)
+    starts_on = models.DateTimeField(default=timezone.now)
+    ends_on = models.DateTimeField()
+    featured_image = models.ImageField(upload_to='featured image')
+    enable = models.BooleanField(default=True)
+	
